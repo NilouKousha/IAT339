5e5a72644ec1375097e1bb376741576277d4475b
Unknown <sahar.jafi@gmail.com>
2017-10-26 17:10:13 -0700

    Cleaned up the main js file

--- pages/js/main.js
+++ pages/js/main.js
@@ -18,62 +18,9 @@ var slidingMenuIsExpanded = false;
 
 if(width < 500){
   	console.log("Window width is less than 500px, collapsing menu.");
-    // navToggle.classList.remove("hidden");
-    // navToggle.setAttribute("aria-hidden", "false");
-    // navItems.classList.add("hidden");
-    // navItems.setAttribute("aria-hidden", "true");
-    // navItems.setAttribute("aria-labelledby", "nav-toggle");
 }
 
-//
-// navToggle.addEventListener("click",
-// 	/*
-// 		Once the user has clicked on the element, we have - in this case - an anonymous (nameless) function that runs a series of statements for us.
-// 	*/
-// 	function() {
-// 		console.log("#nav-toggle has been clicked.");
-//
-// 		/*
-// 			Because we do not know for sure if the navigation items are showing (or not) when a user clicks on the button, we should test and act on both conditions. The if/else statement below checks if #nav-items has a class of 'hidden' using the 'classList.contains(...)' method and acts accordingly.
-// 		*/
-// 		if ( navItems.classList.contains("hidden") ) {
-// 			console.log("#nav-items are hidden, showing them now.");
-//
-// 			/*
-// 				We want to remove the 'hidden' class to ensure that the navigation items become visible.
-// 			*/
-// 			navItems.classList.remove("hidden");
-//
-// 			/*
-// 				Since the menu is now visible, we want to also set the navigation items as visible (to accessibility devices) and the #nav-toggle to understand that the navigation has been expanded.
-// 			*/
-// 			navItems.setAttribute("aria-hidden", "false");
-// 			navToggle.setAttribute("aria-expanded", "true");
-//
-// 			/*
-// 				To assist accessibility devices interpret the menu correctly, refocusing keyboard navigation on the first menu item (once expanded) is worthwhile. This is where selecting our first navigation item in a variable and using the 'focus' method on it allows us to do so.
-// 			*/
-// 			navFirstItem.focus();
-// 		}
-// 		/*
-// 			Otherwise, if the navigation items are already showing...
-// 		*/
-// 		else {
-// 			console.log("#nav-items are showing, hiding them now.");
-//
-// 			/*
-// 				We want to remove add 'hidden' class to ensure that the navigation items are hidden.
-// 			*/
-// 			navItems.classList.add("hidden");
-//
-// 			/*
-// 				Since the menu is now hidden, we want to also set the navigation items as hidden (to accessibility devices) and the #nav-toggle to understand that the navigation has been collapsed.
-// 			*/
-// 			navItems.setAttribute("aria-hidden", "true");
-// 			navToggle.setAttribute("aria-expanded", "false");
-// 		}
-// 	}
-// );
+
 
 
 var hamburgerIcon = document.getElementById('hamburgur-icon');
